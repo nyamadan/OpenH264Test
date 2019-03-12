633d0e5ad75d5fc01fb4637602a86fcefa63ebb4
Yoshiki YAMADA <nyamadandan@gmail.com>
2019-03-12 10:10:05 +0900

    Cleanup Code

--- OpenH264Test/Program.cs
+++ OpenH264Test/Program.cs
@@ -1,32 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OpenH264Test
 {
-    class Program
+    internal class Program
     {
-        const string OpenH264 = "openh264-1.8.0-win64.dll";
+        private const string OpenH264LibraryPath = "openh264-1.8.0-win64.dll";
 
-        [DllImport(OpenH264)]
-        extern public static Int32 WelsCreateSVCEncoder(IntPtr ppEncoder);
+        [DllImport(OpenH264LibraryPath)]
+        public static extern int WelsCreateSVCEncoder(IntPtr ppEncoder);
 
-        [DllImport(OpenH264)]
-        extern public static Int32 WelsDestroySVCEncoder(IntPtr pEncoder);
+        [DllImport(OpenH264LibraryPath)]
+        public static extern int WelsDestroySVCEncoder(IntPtr pEncoder);
 
-        [UnmanagedFunctionPointer(CallingConvention.ThisCall)]
-        delegate Int32 Initialize(IntPtr self, IntPtr param);
-
-        [UnmanagedFunctionPointer(CallingConvention.ThisCall)]
-        delegate Int32 SetOption(IntPtr self, Int32 eOptionId, IntPtr pOption);
-
-        static void Main(string[] args)
+        private static int Main(string[] args)
         {
-            IntPtr pEncoder = IntPtr.Zero;
-            Int32 rv = 0;
+            if (!Environment.Is64BitProcess) return -1;
+
+            var pEncoder = IntPtr.Zero;
+            var rv = 0;
 
             unsafe
             {
@@ -41,18 +33,18 @@ namespace OpenH264Test
             // I420
             unsafe
             {
-                Int32 videoFormat = 23;
+                var videoFormat = 23;
                 rv = _SetOption(pEncoder, 0, new IntPtr(&videoFormat));
             }
 
             // Log: detail
             unsafe
             {
-                Int32 logLevel = 1 << 4;
+                var logLevel = 1 << 4;
                 rv = _SetOption(pEncoder, 25, new IntPtr(&logLevel));
             }
 
-            var param = new Byte[24];
+            var param = new byte[24];
             BitConverter.GetBytes(0).CopyTo(param, 0);
             BitConverter.GetBytes(320).CopyTo(param, 4);
             BitConverter.GetBytes(240).CopyTo(param, 8);
@@ -62,13 +54,21 @@ namespace OpenH264Test
 
             unsafe
             {
-                fixed(Byte *p = param)
+                fixed (byte* p = param)
                 {
                     rv = _Initialize(pEncoder, new IntPtr(p));
                 }
             }
 
             WelsDestroySVCEncoder(pEncoder);
+
+            return 0;
         }
+
+        [UnmanagedFunctionPointer(CallingConvention.ThisCall)]
+        private delegate int Initialize(IntPtr self, IntPtr param);
+
+        [UnmanagedFunctionPointer(CallingConvention.ThisCall)]
+        private delegate int SetOption(IntPtr self, int eOptionId, IntPtr pOption);
     }
 }
