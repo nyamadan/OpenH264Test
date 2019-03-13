174901bd39e5a9af7bb8dd058a382281c99ca524
Yoshiki YAMADA <nyamadandan@gmail.com>
2019-03-13 23:55:59 +0900

    create buffer

--- OpenH264Test/Program.cs
+++ OpenH264Test/Program.cs
@@ -6,13 +6,146 @@ namespace OpenH264Test
 {
     internal class Program
     {
-        private const string OpenH264LibraryPath = "openh264-1.8.0-win64.dll";
+        private const string OpenH264LibraryName = "openh264-1.8.0-win64.dll";
 
-        [DllImport(OpenH264LibraryPath)]
+        [DllImport(OpenH264LibraryName)]
         public static extern int WelsCreateSVCEncoder(IntPtr ppEncoder);
 
-        [DllImport(OpenH264LibraryPath)]
+        [DllImport(OpenH264LibraryName)]
         public static extern int WelsDestroySVCEncoder(IntPtr pEncoder);
+
+        private static byte[] CreateSourcePicture()
+        {
+            var bytes = Environment.Is64BitProcess ? new byte[68] : new byte[52];
+            var offset = 0;
+
+            // int iColorFormat;
+            BitConverter.GetBytes(0).CopyTo(bytes, offset);
+            offset += 4;
+
+            // int iStride[4];
+            for(var i = 0; i < 4; i++)
+            {
+                BitConverter.GetBytes(0).CopyTo(bytes, offset);
+                offset += 4;
+            }
+
+            if(Environment.Is64BitProcess)
+            {
+                // unsigned char* pData[4];
+                for(var i = 0; i < 4; i++)
+                {
+                    BitConverter.GetBytes(0L).CopyTo(bytes, offset);
+                    offset += 8;
+                }
+            } else
+            {
+                // unsigned char* pData[4];
+                for(var i = 0; i < 4; i++)
+                {
+                    BitConverter.GetBytes(0).CopyTo(bytes, offset);
+                    offset += 4;
+                }
+            }
+
+            // int iPicWidth;
+            BitConverter.GetBytes(0).CopyTo(bytes, offset);
+            offset += 4;
+
+            // int iPicHeight;
+            BitConverter.GetBytes(0).CopyTo(bytes, offset);
+            offset += 4;
+
+            // long long uiTimeStamp;
+            BitConverter.GetBytes(0L).CopyTo(bytes, offset);
+            offset += 8;
+
+            return bytes;
+        }
+
+        private static byte[] CreateFrameBSInfo()
+        {
+            var bytes = Environment.Is64BitProcess ? new byte[4116] : new byte[3092];
+            var offset = 0;
+
+            // int iLayerNum;
+            BitConverter.GetBytes(0).CopyTo(bytes, offset);
+            offset += 4;
+
+            // SLayerBSInfo sLayerInfo[MAX_LAYER_NUM_OF_FRAME];
+            for(var i = 0; i < 128; i++)
+            {
+                offset = ApplyLayerInfo(bytes, offset);
+            }
+
+            // EVideoFrameType eFrameType;
+            BitConverter.GetBytes(0).CopyTo(bytes, offset);
+            offset += 4;
+
+            // int iFrameSizeInBytes;
+            BitConverter.GetBytes(0).CopyTo(bytes, offset);
+            offset += 4;
+
+            // long long uiTimeStamp;
+            BitConverter.GetBytes(0L).CopyTo(bytes, offset);
+            offset += 8;
+
+            return bytes;
+        }
+
+        private static int ApplyLayerInfo(byte[] bytes, int offset)
+        {
+            // unsigned char uiTemporalId;
+            bytes[offset] = 0;
+            offset += 1;
+
+            // unsigned char uiSpatialId;
+            bytes[offset] = 0;
+            offset += 1;
+
+            // unsigned char uiQualityId;
+            bytes[offset] = 0;
+            offset += 1;
+
+            // EVideoFrameType eFrameType;
+            BitConverter.GetBytes(0).CopyTo(bytes, offset);
+            offset += 4;
+
+            // unsigned char uiLayerType;
+            bytes[offset] = 0;
+            offset += 1;
+
+            // int iSubSeqId;
+            BitConverter.GetBytes(0).CopyTo(bytes, offset);
+            offset += 4;
+
+            // int iNalCount;
+            BitConverter.GetBytes(0).CopyTo(bytes, offset);
+            offset += 4;
+
+            if(Environment.Is64BitProcess)
+            {
+                // int* pNalLengthInByte;
+                BitConverter.GetBytes(0L).CopyTo(bytes, offset);
+                offset += 8;
+
+                // unsigned char* pBsBuf;
+                BitConverter.GetBytes(0L).CopyTo(bytes, offset);
+                offset += 8;
+            }
+            else
+            {
+                // int* pNalLengthInByte;
+                BitConverter.GetBytes(0).CopyTo(bytes, offset);
+                offset += 4;
+
+                // unsigned char* pBsBuf;
+                BitConverter.GetBytes(0).CopyTo(bytes, offset);
+                offset += 4;
+            }
+
+            return offset;
+        }
 
         private static int Main(string[] args)
         {
