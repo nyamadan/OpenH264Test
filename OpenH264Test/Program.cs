c0a34e27a867e2c90c5b4e52293608c0656f6fcd
Yoshiki YAMADA <nyamadandan@gmail.com>
2019-03-22 23:48:18 +0900

    iroiro

--- OpenH264Test/Program.cs
+++ OpenH264Test/Program.cs
@@ -152,9 +152,14 @@ namespace OpenH264Test
         [DllImport(OpenH264LibraryName)]
         public static extern void WelsDestroySVCEncoder(IntPtr pEncoder);
 
-        private static unsafe int Mp4H26xWriteNal(byte* nal, int length, uint timeStamp90kHz_next)
+        const int MINIMP4_MAX_SPS = 32;
+        const int MINIMP4_MAX_PPS = 256;
+
+        private static unsafe int Mp4H26xWriteNal(mp4_h26x_writer_t h, byte* nal, int length, uint timeStamp90kHz_next)
         {
-            // WIP
+            const int MP4E_SAMPLE_DEFAULT = 0;   // (beginning of) audio or video frame
+            const int MP4E_SAMPLE_RANDOM_ACCESS = 1;   // mark sample as random access point (key frame)
+            const int MP4E_SAMPLE_CONTINUATION = 2;   // Not a sample, but continuation of previous sample (new slice)
 
             var eof = nal + length;
             var sizeof_nal = 0;
@@ -176,14 +181,163 @@ namespace OpenH264Test
                 fixed (byte* nal2 = _nal2)
                 {
                     sizeof_nal = remove_nal_escapes(nal1, nal, sizeof_nal);
+                    sizeof_nal = transcode_nalu(h.sps_patcher, nal2, sizeof_nal, nal1);
+
                     if (sizeof_nal == 0)
                     {
                         return 0;
+                    }
+
+                    switch (payload_type)
+                    {
+                        case 7:
+                            MP4E__set_sps(h.mux, h.mux_track_id, nal2 + 4, sizeof_nal - 4);
+                            h.need_sps = 0;
+                            break;
+
+                        case 8:
+                            if (h.need_sps != 0)
+                                return 0;
+                            MP4E__set_pps(h.mux, h.mux_track_id, nal2 + 4, sizeof_nal - 4);
+                            h.need_pps = 0;
+                            break;
+
+                        case 5:
+                            if (h.need_sps != 0)
+                                return 0;
+
+                            h.need_idr = 0;
+
+                            if (h.need_sps != 0)
+                                return 0;
+
+                            if (h.need_pps == 0 && h.need_idr == 0)
+                            {
+                                int sample_kind = MP4E_SAMPLE_DEFAULT;
+                                nal2[0] = (byte)((sizeof_nal - 4) >> 24);
+                                nal2[1] = (byte)((sizeof_nal - 4) >> 16);
+                                nal2[2] = (byte)((sizeof_nal - 4) >> 8);
+                                nal2[3] = (byte)((sizeof_nal - 4));
+                                if (payload_type == prev_payload_type)
+                                {
+                                    sample_kind = MP4E_SAMPLE_CONTINUATION;
+                                }
+                                else if (payload_type == 5)
+                                {
+                                    sample_kind = MP4E_SAMPLE_RANDOM_ACCESS;
+                                }
+                                prev_payload_type = payload_type;
+                                MP4E__put_sample(h.mux, h.mux_track_id, nal2, sizeof_nal, timeStamp90kHz_next, sample_kind);
+                            }
+                            break;
+
+                        default:
+                            if (h.need_sps != 0)
+                                return 0;
+                            if (h.need_pps == 0 && h.need_idr == 0)
+                            {
+                                int sample_kind = MP4E_SAMPLE_DEFAULT;
+                                nal2[0] = (byte)((sizeof_nal - 4) >> 24);
+                                nal2[1] = (byte)((sizeof_nal - 4) >> 16);
+                                nal2[2] = (byte)((sizeof_nal - 4) >> 8);
+                                nal2[3] = (byte)((sizeof_nal - 4));
+                                if (payload_type == prev_payload_type)
+                                {
+                                    sample_kind = MP4E_SAMPLE_CONTINUATION;
+                                }
+                                else if (payload_type == 5)
+                                {
+                                    sample_kind = MP4E_SAMPLE_RANDOM_ACCESS;
+                                }
+                                prev_payload_type = payload_type;
+                                MP4E__put_sample(h.mux, h.mux_track_id, nal2, sizeof_nal, timeStamp90kHz_next, sample_kind);
+                            }
+                            break;
                     }
                 }
             }
 
             return 1;
+        }
+
+        public enum track_media_kind
+        {
+            e_audio,
+            e_video,
+            e_private
+        }
+
+        public class MP4E_track_t
+        {
+            // MP4 object type code, which defined codec class for the track.
+            // See MP4E_OBJECT_TYPE_* values for some codecs
+            public uint object_type_indication;
+
+            // Track language: 3-char ISO 639-2T code: "und", "eng", "rus", "jpn" etc...
+            public byte[] language = new byte[4];
+
+            public track_media_kind track_media_kind;
+
+            // 90000 for video, sample rate for audio
+            public uint time_scale;
+            public uint default_duration;
+
+            public uint channelcount
+            {
+                get { return (uint)(0xffffffff & u); }
+                set { u = (height << 32) | (int)value; }
+            }
+
+            public int width
+            {
+                get { return (int)(0xffffffff & u); }
+                set { u = (height << 32) | value; }
+            }
+
+            public int height
+            {
+                get { return (int)(0xffffffff & (u >> 32)); }
+                set { u = (value << 32) | width; }
+            }
+
+            public long u;
+        }
+
+        public class minimp4_vector_t
+        {
+            byte[] data;
+            int bytes;
+            int capacity;
+        }
+
+        public class MP4E_mux_t
+        {
+            minimp4_vector_t tracks;
+
+            long write_pos;
+            long mdat_pos;
+
+            string text_comment;
+
+            int sequential_mode_flag;
+        }
+
+        public class mp4_h26x_writer_t
+        {
+            public h264_sps_id_patcher_t sps_patcher;
+            public MP4E_mux_t mux;
+            public int mux_track_id, is_hevc, need_vps, need_sps, need_pps, need_idr;
+        }
+
+        public class h264_sps_id_patcher_t
+        {
+            public byte[][] sps_cache = new byte[MINIMP4_MAX_SPS][];
+            public byte[][] pps_cache = new byte[MINIMP4_MAX_PPS][];
+            public int[] sps_bytes = new int[MINIMP4_MAX_SPS];
+            public int[] pps_bytes = new int[MINIMP4_MAX_PPS];
+
+            public int[] map_sps = new int[MINIMP4_MAX_SPS];
+            public int[] map_pps = new int[MINIMP4_MAX_SPS];
         }
 
         public class bit_reader_t
@@ -391,41 +545,36 @@ namespace OpenH264Test
             set_pos_bits(bs, 0);
         }
 
-        static unsafe bool memcmp<T>(T[] a, T[] b, int length) where T : IComparable<T>
+        static unsafe bool memcmp(void* a, byte[] b, int length)
         {
             for (var i = 0; i < length; i++)
             {
-                if(a[i].CompareTo(b[i]) != 0)
+                if (((byte*)a)[i] != b[i])
                 {
                     return false;
                 }
             }
 
             return true;
+        }
+
+        static unsafe void memcpy(byte[] dst, void* src, int length)
+        {
+            for (var i = 0; i < length; i++)
+            {
+                dst[i] = ((byte*)src)[i];
+            }
         }
 
         static unsafe void memcpy(void* dst, void* src, int length)
         {
             for (var i = 0; i < length; i++)
             {
-                ((byte *)dst)[i] = ((byte *)src)[i];
+                ((byte*)dst)[i] = ((byte*)src)[i];
             }
         }
 
-        static unsafe bool memcmp(void* a, void* b, int length)
-        {
-            for (var i = 0; i < length; i++)
-            {
-                if (*((byte*)a + i) != *((byte*)b + i))
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        static unsafe int find_mem_cache(void*[] cache, int[] cache_bytes, int cache_size, void* mem, int bytes)
+        static unsafe int find_mem_cache(byte[][] cache, int[] cache_bytes, int cache_size, void* mem, int bytes)
         {
             int i;
 
@@ -445,7 +594,7 @@ namespace OpenH264Test
             {
                 if (cache_bytes[i] == 0)
                 {
-                    cache[i] = malloc(bytes);
+                    cache[i] = new byte[bytes];
                     if (cache[i] != null)
                     {
                         memcpy(cache[i], mem, bytes);
@@ -457,8 +606,45 @@ namespace OpenH264Test
             return -1; // no room
         }
 
+        private static unsafe int patch_pps(h264_sps_id_patcher_t h, bit_reader_t bs, bs_t bd, int new_pps_id, int* old_id)
+        {
+            int bytes;
+            uint pps_id = (uint)ue_bits(bs);  // max = 255
+            uint sps_id = (uint)ue_bits(bs);  // max = 31
 
-        private static unsafe int transcode_nalu(byte* src, int nalu_bytes, byte* dst)
+            *old_id = (int)pps_id;
+            sps_id = (uint)h.map_sps[sps_id];
+            pps_id = (uint)new_pps_id;
+
+            Debug.Assert(sps_id <= 31);
+            Debug.Assert(pps_id <= 255);
+
+            h264e_bs_put_golomb(bd, pps_id);
+            h264e_bs_put_golomb(bd, sps_id);
+            copy_bits(bs, bd);
+
+            bytes = (int)h264e_bs_byte_align(bd) / 8;
+            h264e_bs_flush(bd);
+            return bytes;
+        }
+
+        private static unsafe void patch_slice_header(h264_sps_id_patcher_t h, bit_reader_t bs, bs_t bd)
+        {
+            uint first_mb_in_slice = (uint)ue_bits(bs);
+            uint slice_type = (uint)ue_bits(bs);
+            uint pps_id = (uint)ue_bits(bs);
+
+            pps_id = (uint)h.map_pps[pps_id];
+
+            Debug.Assert(pps_id <= 255);
+
+            h264e_bs_put_golomb(bd, first_mb_in_slice);
+            h264e_bs_put_golomb(bd, slice_type);
+            h264e_bs_put_golomb(bd, pps_id);
+            copy_bits(bs, bd);
+        }
+
+        private static unsafe int transcode_nalu(h264_sps_id_patcher_t h, byte* src, int nalu_bytes, byte* dst)
         {
             int old_id;
 
@@ -480,24 +666,24 @@ namespace OpenH264Test
                 case 7:
                     {
                         int cb = change_sps_id(bst, bdt, 0, &old_id);
-                        int id = find_mem_cache(h->sps_cache, h->sps_bytes, 32, dst + 1, cb);
+                        int id = find_mem_cache(h.sps_cache, h.sps_bytes, MINIMP4_MAX_SPS, dst + 1, cb);
                         if (id == -1)
                         {
                             return 0;
                         }
-                        h->map_sps[old_id] = id;
+                        h.map_sps[old_id] = id;
                         change_sps_id(bs, bd, id, &old_id);
                     }
                     break;
                 case 8:
                     {
                         int cb = patch_pps(h, bst, bdt, 0, &old_id);
-                        int id = find_mem_cache(h->pps_cache, h->pps_bytes, 256, dst + 1, cb);
+                        int id = find_mem_cache(h.pps_cache, h.pps_bytes, MINIMP4_MAX_PPS, dst + 1, cb);
                         if (id == -1)
                         {
                             return 0;
                         }
-                        h->map_pps[old_id] = id;
+                        h.map_pps[old_id] = id;
                         patch_pps(h, bs, bd, id, &old_id);
                     }
                     break;
